5db205b55051f98820277d636e02a37e9e2e9fe7
markandrada <110426094+markandrada@users.noreply.github.com>
2022-08-18 09:21:09 -0300

    Se agrego el código de index.js
    Código para validar el inicio de sesión, en caso de ser validada redirigir la pagina al home.html, en caso contrario mostrara los campos que faltan rellenar.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,30 @@
+const emailError = document.querySelector('#floatingInput + span.error');
+const contraseñaError = document.querySelector('#floatingPassword + span.error');
+const emailCuadro = document.getElementById("floatingInput");
+const contraseñaCuadro = document.getElementById("floatingPassword");
+document.getElementById("login_button").addEventListener("click", validacion);
 
+function validacion(){
+    var mail, contraseña;
+    mail = document.getElementById("floatingInput").value;
+    contraseña = document.getElementById("floatingPassword").value;
+    emailError.innerHTML = '';
+    contraseñaError.innerHTML = '';
+    emailCuadro.style.borderColor = "";
+    contraseñaCuadro.style.borderColor = "";
+    if (mail=='' || contraseña==''){
+        if(mail==''){
+            emailError.textContent = 'Ingrese tu e-mail';
+            emailCuadro.style.borderColor = "red";
+        }
+        if(contraseña==''){
+            contraseñaError.textContent = 'Ingrese tu contraseña';
+            contraseñaCuadro.style.borderColor = "red";
+        }
+    }
+    else{
+        window.location.href ="home.html";
+    }
+    
+
+}
