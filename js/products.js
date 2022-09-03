931adb89707a1094d3bd46a9420cb657f37730df
markandrada <110426094+markandrada@users.noreply.github.com>
2022-09-02 22:33:53 -0300

    Update products.js
    Todo el archivo modificado.

--- js/products.js
+++ js/products.js
@@ -1,40 +1,135 @@
+let minCost = undefined;
+let maxCost = undefined;
 let categoriesArray = [];
+
+const ORDER_ASC_BY_COST = "09";
+const ORDER_DESC_BY_COST = "90";
+const ORDER_BY_PROD_COUNT = "Cant.";
+let currentCategoriesArray = [];
+let currentSortCriteria = undefined;
 
 function showCategoriesList(array){
     let htmlContentToAppend = "";
 
-    for(let i = 0; i < array.products.length; i++){ 
-        let category = array.products[i];
-        htmlContentToAppend += `
-        <div class="list-group-item list-group-item-action">
-            <div class="row">
-                <div class="col-3">
-                    <img src="` + category.image + `" alt="product image" class="img-thumbnail">
-                </div>
-                <div class="col">
-                    <div class="d-flex w-100 justify-content-between">
-                        <div class="mb-1">
-                        <h4>`+ category.name +` - ` + category.currency + ` ` + category.cost + `</h4> 
-                        <p> `+ category.description +`</p> 
-                        </div>
-                        <small class="text-muted">` + category.soldCount + ` artículos</small> 
-                    </div>
+    for(let i = 0; i < array.length; i++){ 
+        let category = array[i];
+        if (((minCost == undefined) || (minCost != undefined && parseInt(category.cost) >= minCost)) &&
+            ((maxCost == undefined) || (maxCost != undefined && parseInt(category.cost) <= maxCost))){
 
+                htmlContentToAppend += `
+                <div class="list-group-item list-group-item-action">
+                    <div class="row">
+                        <div class="col-3">
+                            <img src="` + category.image + `" alt="product image" class="img-thumbnail">
+                        </div>
+                        <div class="col">
+                            <div class="d-flex w-100 justify-content-between">
+                                <div class="mb-1">
+                                <h4>`+ category.name +` - ` + category.currency + ` ` + category.cost + `</h4> 
+                                <p> `+ category.description +`</p> 
+                                </div>
+                                <small class="text-muted">` + category.soldCount + ` artículos</small> 
+                            </div>
+        
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-        `
+                `
+        }
+        document.getElementById("cat-list-container").innerHTML = htmlContentToAppend; 
     }
-    document.getElementById("cat-list-container").innerHTML = htmlContentToAppend; 
+    
 }
 
+let product_id = localStorage.getItem("catID");
 
 document.addEventListener("DOMContentLoaded", function(e){
-    getJSONData(CAR_URL).then(function(resultObj){
+    getJSONData(PRODUCTS_URL+product_id+EXT_TYPE).then(function(resultObj){
         if (resultObj.status === "ok")
         {
             categoriesArray = resultObj.data;
-            showCategoriesList(categoriesArray);
+            showCategoriesList(categoriesArray.products);
         }
     });
+
+    document.getElementById("clearRangeFilter").addEventListener("click", function(){
+        document.getElementById("rangeFilterCostMin").value = "";
+        document.getElementById("rangeFilterCostMax").value = "";
+
+        minCost = undefined;
+        maxCost = undefined;
+
+        showCategoriesList(categoriesArray.products);
+    });
+    
+    document.getElementById("rangeFilterCost").addEventListener("click", function(){
+    //Obtengo el mínimo y máximo de los intervalos para filtrar por cantidad
+    //de productos por categoría.
+        minCost = document.getElementById("rangeFilterCostMin").value;
+        maxCost = document.getElementById("rangeFilterCostMax").value;
+
+        if ((minCost != undefined) && (minCost != "") && (parseInt(minCost)) >= 0){
+            minCost = parseInt(minCost);
+        }
+        else{
+            minCost = undefined;
+        }
+
+        if ((maxCost != undefined) && (maxCost != "") && (parseInt(maxCost)) >= 0){
+            maxCost = parseInt(maxCost);
+        }
+        else{
+            maxCost = undefined;
+        }
+
+        showCategoriesList(categoriesArray.products);
+    });
+});
+
+
+function sortCategories(criteria, array){
+    let result = [];
+    if (criteria === ORDER_ASC_BY_COST)
+    {
+        result = array.products.sort(function(a, b) {
+            return a.cost - b.cost;
+        });
+    }else if (criteria === ORDER_DESC_BY_COST){
+        result = array.products.sort(function(a, b) {
+            return b.cost - a.cost;
+        });
+    }else if (criteria === ORDER_BY_PROD_COUNT){
+        result = array.products.sort(function(a, b) {
+            let aCount = parseInt(a.soldCount);
+            let bCount = parseInt(b.soldCount);
+
+            return bCount - aCount;
+        });
+    }
+    return result;
+}
+
+function sortAndShowCategories(sortCriteria, categoriesArray){
+    currentSortCriteria = sortCriteria;
+
+    if(categoriesArray != undefined){
+        currentCategoriesArray = categoriesArray;
+    }
+
+    currentCategoriesArray = sortCategories(currentSortCriteria, currentCategoriesArray);
+
+    //Muestro las categorías ordenadas
+    showCategoriesList(currentCategoriesArray);
+}
+
+document.getElementById("sortAsc").addEventListener("click", function(){
+    sortAndShowCategories(ORDER_ASC_BY_COST, categoriesArray);
+});
+
+document.getElementById("sortDesc").addEventListener("click", function(){
+    sortAndShowCategories(ORDER_DESC_BY_COST, categoriesArray);
+});
+
+document.getElementById("sortByRel").addEventListener("click", function(){
+    sortAndShowCategories(ORDER_BY_PROD_COUNT, categoriesArray);
 });
