f15d24c74c78c1db5e00c46a2d14e8e60ea23d4e
markandrada <110426094+markandrada@users.noreply.github.com>
2022-11-01 20:30:40 -0300

    Update my-profile.js
    Línea 27: Se agrego código para cuando la imagen este en LocalStorage
    Línea 70: Se agrego código para modificar la imagen de perfil y guardarla en LocalStorage

--- js/my-profile.js
+++ js/my-profile.js
@@ -6,6 +6,13 @@ let mail = document.getElementById("profile_mail");
 let cnumber = document.getElementById("profile_contact_number");
 let user_mail = localStorage.getItem("Usuario");
 
+
+//Funcion que se ejecuta al abrir la pagina. Se fija si el usuario esta vacio.
+//En caso de estarlo, oculta la pagina de perfil
+//En caso de no estarlo. Muestra los datos ya guardados con anterioridad
+//Al igual que la imagen de perfil
+
+
 (function user_empty(){
     if(user_mail==null){
         document.getElementById("profile_container").classList.add('d-none');
@@ -17,28 +24,15 @@ let user_mail = localStorage.getItem("Usuario");
         ssname.value=localStorage.getItem("Second_Surname");
         mail.value=user_mail;
         cnumber.value=localStorage.getItem("Contact_Number");
-        'use strict'
-
-        // Fetch all the forms we want to apply custom Bootstrap validation styles to
-        var forms = document.querySelectorAll('.needs-validation')
-        const terminosElem = document.getElementById('terminos')
-
-        // Loop over them and prevent submission
-        Array.prototype.slice.call(forms)
-            .forEach(function (form) {
-                form.addEventListener('submit', function (event) {
-
-                    if (!form.checkValidity() || terminosElem.validity.valueMissing) {
-                        event.preventDefault()
-                        event.stopPropagation()
-                    }
-
-                    form.classList.add('was-validated')
-                }, false)
-            })
+        let pimage = localStorage.getItem("Profile_image");
+        if(pimage!=null){
+            document.getElementById("profile_picture").src = pimage;
+        }
     }
-    
 })
+
+//Se ejecuta tambien al abrir la pagina, se encarga de tirar los mensajes de error
+//Cuando no se han rellenado los campos obligatorios
 
 (function () {
     'use strict'
@@ -62,6 +56,8 @@ let user_mail = localStorage.getItem("Usuario");
         })
 })
 
+//Guarda los valores en el LocalStorage de los campos que se hayan llenado
+
 function validate(){
     localStorage.setItem("First_Name", fname.value);
     localStorage.setItem("Second_Name", sname.value);
@@ -69,3 +65,11 @@ function validate(){
     localStorage.setItem("Second_Surname", ssname.value);
     localStorage.setItem("Contact_Number",cnumber.value);
 }
+//Cambia la imagen de perfil y guarda la imagen en el LocalHost
+
+function loadFile(event) {
+    var image = document.getElementById("profile_picture");
+    image.src = URL.createObjectURL(event.target.files[0]);
+    localStorage.setItem("Profile_image", image.src);
+  };
+  
